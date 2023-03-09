055f1c3a454fdc0fdf6544a7958979c95140c026
SoTrabalhoPUC <127440987+SoTrabalhoPUC@users.noreply.github.com>
2023-03-09 19:53:25 -0300

    Create SomarDoisNumeros.java

---
+++ SomarDoisNumeros.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+/*
+ * @author gabrielribeirotodtferreira
+ *
+ */
+class SomarDoisNumeros {
+	/*
+	 * 
+	 * @param args
+	 */
+	
+	
+	public static void main(String[] args){
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int entrada1 = sc.nextInt();
+		int entrada2 = sc.nextInt();
+		System.out.println(entrada1+entrada2);
+		sc.close();
+	}
+	
+}
